1df66280019648f0f0dce70c4a2d3fc668fdeeeb
yutveg <kodi.0n31l@gmail.com>
2020-04-10 10:11:16 -0700

    oop tests passed

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,21 +3,31 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
+
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
+    def drive(self):
+        return "vroooom"
     # TODO
 
 
 # Subclass Motorcycle from GroundVehicle.
 #
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__(2)
+
+    def drive(self):
+        return "BRAAAP!!"
 # Make it so when you instantiate a Motorcycle, it automatically sets the number
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 # TODO
+
 
 vehicles = [
     GroundVehicle(),
